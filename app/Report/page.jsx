07dac7930a0577ae7de5b1d2927762726d7d1aa0
Rudalph <gonsalvesrudalph@gmail.com>
2024-07-18 13:54:24 +0530

    Endpoint updates

--- app/Report/page.jsx
+++ app/Report/page.jsx
@@ -28,7 +28,7 @@ const Page = () => {
     formData.append('file', file);
 
     try {
-      const response = await fetch('http://localhost:5001/upload', {
+      const response = await fetch('http://localhost:5004/upload', {
         method: 'POST',
         body: formData,
       });
