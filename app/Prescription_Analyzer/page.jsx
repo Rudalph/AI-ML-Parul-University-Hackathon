64dde26753b5517a08bb41b9c22009c90353a4dc
Rudalph <gonsalvesrudalph@gmail.com>
2024-07-19 16:05:45 +0530

    updates

--- app/Prescription_Analyzer/page.jsx
+++ app/Prescription_Analyzer/page.jsx
@@ -42,7 +42,7 @@ const Page = () => {
     const handleSubmit = async () => {
         console.log(question)
         try {
-            const response = await fetch('http://localhost:5005/brand', {
+            const response = await fetch('http://localhost:5006/brand', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
